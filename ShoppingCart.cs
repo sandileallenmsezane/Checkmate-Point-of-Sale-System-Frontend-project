27f3f89e4e75aae93b8ec96c11726297ce039950
sandile-21 <129226414+sandile-21@users.noreply.github.com>
2024-02-02 11:54:18 +0200

    Add files via upload

---
+++ ShoppingCart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProjectM4
+{
+    public class ShoppingCart
+    {
+       public int Prod_Id { get; set; }
+
+      public  String Prod_Name { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal SubTotal { get; set; }
+
+
+    }
+}
